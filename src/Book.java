0746aba7e6693fd6734ad705054feb0cd24624e9
Samuel20072 <samueltorresospina321@gmail.com>
2024-08-15 09:00:22 -0500

    Book

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,27 @@
-public class Book {
+import java.util.*;
 
+class Book extends Media {
+    private Date editionDate;
+    private String editorial;
+    private String isbn;
+    private List<String> authors;
+
+    public Book(int id, String title, Date editionDate, String editorial, String isbn) {
+        super(id, title);
+        this.editionDate = editionDate;
+        this.editorial = editorial;
+        this.isbn = isbn;
+        this.authors = new ArrayList<>();
+    }
+
+    public void addAuthor(String author) {
+        authors.add(author);
+    }
+
+    @Override
+    public void view(int minutes) {
+        this.viewed = true;
+        this.timeViewed = minutes;
+        System.out.println("Has leído el libro: " + title + " durante " + minutes + " minutos.");
+    }
 }
